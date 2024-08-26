d6c313ad5f78ca041709dedf49da93af4ad7c9d0
lutri <lutringerkevin@gmail.com>
2024-08-26 13:38:22 +0200

    ajout Mongoose et Route

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
-app.use((req, res, next) =>{
-    console.log('requête reçue !');
-    next();
-});
+
+mongoose.connect('mongodb+srv://A-Linxear:QHETnMg2FpqFUDEj@cluster0.bugxm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+      useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'
+));
 
 app.use((req, res, next) => {
-    res.status(201);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next)=> {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-});
+app.use(express.json());
 
-app.use((req, res) => {
-    console.log('réponse envoyée avec succès !');
-});
+app.use('/api/book', bookRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
