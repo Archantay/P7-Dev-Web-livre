e9a342b52494128f37483ae3a46533ad3b4615b8
lutri <lutringerkevin@gmail.com>
2024-09-25 12:33:15 +0200

    ajout du middleware sharp dans les routes Post et Put

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -2,14 +2,17 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
+const sharp = require('../middleware/sharp');
 
 const bookCtrl = require('../controllers/book');
 
-router.post('/', auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.post('/', auth, multer, sharp, bookCtrl.createBook);
+router.put('/:id', auth, multer, sharp, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
+router.get('/bestrating', bookCtrl.getBestRatedBooks);
 router.get('/:id', bookCtrl.getOneBook);
 router.get('/', bookCtrl.getAllBook);
-router.post('/books/:id/rating', auth, bookCtrl.rateBook);
- 
+router.post('/:id/rating', auth, bookCtrl.rateBook);
+
+
 module.exports = router;
